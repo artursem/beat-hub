c4bb7503780ca272b91c1c2150e1ef352f91f94a
artursem <artur.semeniuk@gmail.com>
2022-03-22 19:55:38 +0100

    library skeleton

--- src/components/library/SkeletonLibrary.tsx
+++ src/components/library/SkeletonLibrary.tsx
@@ -1,4 +1,3 @@
-import { FC } from 'react';
 import Li from 'src/elements/text/Li';
 import List from 'src/elements/text/List';
 import { Skeleton } from '@chakra-ui/react';
@@ -8,16 +7,8 @@ interface SkeletonLibraryProps {
 }
 
 const SkeletonLibrary = ({ length }: SkeletonLibraryProps) => {
-	// const skeleton = (
-	// 	<Skeleton
-	// 		width={{ base: 152, md: '100%', '2xl': 152 }}
-	// 		height={{ base: 250, md: '102px', '2xl': 250 }}
-	// 		marginY={3}
-	// 		marginX={{ base: 3, md: 0, '2xl': 3 }}
-	// 		key={i}
-	// 	/>
-	// );
 	const skeletonLis = Array.of(length).map((x, i) => (
+		// <Li key={i}>
 		<Skeleton
 			width={{ base: 152, md: '100%', '2xl': 152 }}
 			height={{ base: 250, md: '102px', '2xl': 250 }}
@@ -25,13 +16,9 @@ const SkeletonLibrary = ({ length }: SkeletonLibraryProps) => {
 			marginX={{ base: 3, md: 0, '2xl': 3 }}
 			key={i}
 		/>
+		// </Li>
 	));
-	return (
-		<List>
-			{skeletonLis}
-			{/* <Li>{skeleton}</Li> */}
-		</List>
-	);
+	return <List>{skeletonLis}</List>;
 };
 
 export default SkeletonLibrary;
