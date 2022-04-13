fa98432b403cd16683bc2ce23d4b7a0d590fa5cd
artursem <artur.semeniuk@gmail.com>
2022-04-13 22:19:41 +0200

    change 404 image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,13 @@
 import Image from 'next/image';
-import brokenRecord from '../../public/404.png';
+// import brokenRecord from '../../public/404.png';
+import brokenRecord from '../../public/404_2.png';
 import HeadingPrimary from 'src/components/headings/HeadingPrimary';
 import Center from 'src/components/layout/Center';
 export default function Custom404() {
 	return (
 		<>
 			<HeadingPrimary>Page Not Found</HeadingPrimary>
-			<Center>
+			<Center paddingTop='25px'>
 				<Image src={brokenRecord} alt='Page not found' />
 			</Center>
 		</>
