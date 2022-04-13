03ed8bc301f60eef10c35e82eed633910e12f54e
artursem <artur.semeniuk@gmail.com>
2022-04-13 21:57:18 +0200

    add 404 page image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,14 @@
+import Image from 'next/image';
+import brokenRecord from '../../public/404.png';
 import HeadingPrimary from 'src/components/headings/HeadingPrimary';
+import Center from 'src/components/layout/Center';
 export default function Custom404() {
-	return <HeadingPrimary>Page Not Found</HeadingPrimary>;
+	return (
+		<>
+			<HeadingPrimary>Page Not Found</HeadingPrimary>
+			<Center>
+				<Image src={brokenRecord} alt='Page not found' />
+			</Center>
+		</>
+	);
 }
