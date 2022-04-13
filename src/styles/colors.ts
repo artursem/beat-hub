a33b2b6bb1ee3c32f37c50ce8a41ff93fe272147
artursem <artur.semeniuk@gmail.com>
2022-04-13 19:11:04 +0200

    global colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,27 +1,3 @@
-export const DARK = {
-	text: 'gray.300',
-	bg: 'gray.800',
-	hoverText: 'gray.200',
-	hoverBg: 'gray.700',
-	border: 'gray.700',
-	gradient: 'linear(to-bl, gray.700, gray.800)',
-	input: 'gray.100',
-	inputBg: 'gray.800',
-	placeholder: 'gray.100',
-};
-
-export const LIGHT = {
-	text: 'gray.800',
-	bg: 'gray.50',
-	hoverText: 'gray.800',
-	hoverBg: 'gray.100',
-	border: 'gray.300',
-	gradient: 'linear(to-bl, gray.200, gray.400)',
-	input: 'gray.900',
-	inputBg: 'gray.50',
-	placeholder: 'gray.900',
-};
-
 interface Color {
 	text: [string, string];
 	bg: [string, string];
