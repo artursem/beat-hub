4d407b59854cf8dc8770e3dc345c35bbc92f961f
artursem <artur.semeniuk@gmail.com>
2022-04-26 21:45:51 +0200

    add library tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -17,6 +17,9 @@ context('Search feature', () => {
 		cy.get('input').type('the beatles');
 		cy.contains('The Beatles').click();
 		cy.url().should('include', '/artist/art.61025');
+		cy.location().should((location) => {
+			expect(location.pathname).to.eq('/artist/art.61025');
+		});
 		cy.get('h2').should('contain', 'The Beatles');
 	});
 });
@@ -35,5 +38,30 @@ context('Add to library feature', () => {
 	});
 });
 
-// view library
+context('Library page', () => {
+	beforeEach(() => {
+		cy.visit('http://localhost:3000/artist/art.61025');
+		cy.contains('Add to Library').click();
+		cy.visit('http://localhost:3000/library');
+	});
+	it('has artist in localStorage', () => {
+		expect(localStorage.getItem('library')).to.eq('["art.61025"]');
+	});
+	it('displays library', () => {
+		cy.get('a').should('contain', 'The Beatles');
+	});
+	it('removes artist from library and localStorage', () => {
+		cy.contains('In your Library')
+			.click()
+			.should(() => {
+				expect(localStorage.getItem('library')).to.eq('[]');
+			});
+		cy.get('h3').contains('Your library is empty. Please add artists you enjoy');
+	});
+});
+
+// view library, remove artist
+// navigation
+// cy.go(-1)
+//     cy.location('pathname').should('not.include', 'navigation')
 // darkmode
