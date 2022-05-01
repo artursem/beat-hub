9d7feeae427d5dbd8c55bbaae683645dd9bca883
artursem <artur.semeniuk@gmail.com>
2022-05-01 19:57:02 +0200

    add test

--- src/sections/app/musicSearch/DisplayList.tsx
+++ src/sections/app/musicSearch/DisplayList.tsx
@@ -1,25 +1,10 @@
-import { useEffect } from 'react';
-import { useAppSelector, useAppDispatch } from 'src/store/hooks';
-import {
-	// fetchSearchImages,
-	// selectSearchImages,
-
-	selectSearchResult,
-} from 'src/store/search/search-slice';
+import { useAppSelector } from 'src/store/hooks';
+import { selectSearchResult } from 'src/store/search/search-slice';
 import OptionItem from './OptionItem';
 import ListBox from 'src/components/text/ListBox';
 
 const DisplayList = () => {
 	const searchList = useAppSelector(selectSearchResult);
-	// const searchImages = useAppSelector(selectSearchImages);
-	// const dispatch = useAppDispatch();
-
-	// useEffect(() => {
-	// 	if (searchList) {
-	// 		dispatch(fetchDataAndThumbnails(idList));
-	// 	}
-	// }, [dispatch, searchList]);
-
 	const showArtist =
 		searchList &&
 		searchList.map(({ name, id, thumbnail }) => {
--- tests/sections/app/musicSearch/SearchBox.test.tsx
+++ tests/sections/app/musicSearch/SearchBox.test.tsx
@@ -1,15 +1,22 @@
 import React, { ReactElement } from 'react';
 import { Provider } from 'react-redux';
 import store from 'src/store/store';
-import { screen, render as rtlRender } from '@testing-library/react';
+import { screen, render as rtlRender, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SearchBox from 'src/sections/app/musicSearch/SearchBox';
 
 const render = (component: ReactElement) =>
 	rtlRender(<Provider store={store}>{component}</Provider>);
 
-test('Search for artist - hp', () => {
+test('Search for artist - hp', async () => {
 	render(<SearchBox />);
-	const textInput = screen.getByRole('textbox', { name: 'Search' });
-	expect(textInput).toBeInTheDocument();
+	const searchInput = screen.getByRole('textbox', { name: 'Search' });
+	expect(searchInput).toBeInTheDocument();
+	userEvent.clear(searchInput);
+	userEvent.type(searchInput, 'The Beatles');
+	const spinner = screen.getByRole('');
+	// await waitFor(async () => {
+	// 	const resultList = await screen.findByRole('list');
+	// 	expect(resultList).toHaveLength(5);
+	// });
 });
